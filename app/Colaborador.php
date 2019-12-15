2a7127d49828e6da1a85958598ff8fe29ccc4278
aldo1400 <achoque1400@gmail.com>
2019-12-15 11:16:59 -0500

    swagger actualizado

--- app/Colaborador.php
+++ app/Colaborador.php
@@ -53,6 +53,15 @@ class Colaborador extends Model
     ];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
      * The attributes that should be mutated to dates.
      *
      * @var array
