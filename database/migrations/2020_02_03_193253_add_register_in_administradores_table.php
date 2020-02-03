5893f1fb18861af3d0ad8ec147b0e51d9568d2a9
aldo1400 <achoque1400@gmail.com>
2020-02-03 14:35:38 -0500

    primer admin agregado

---
+++ database/migrations/2020_02_03_193253_add_register_in_administradores_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRegisterInAdministradoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('administradores')->insert([
+            [
+                'nombre' => 'Administrador',
+                'username' => 'admin',
+                'password'=>Hash::make('admin'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('administradores', function (Blueprint $table) {
+            //
+        // });
+    }
+}
