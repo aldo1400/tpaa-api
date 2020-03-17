fc15fa72cd12824086694961d56196bbc3234d7f
aldo1400 <achoque1400@gmail.com>
2020-03-16 22:30:30 -0500

    movilidades ordenadas por fecha de inicio

--- app/Http/Controllers/Colaboradores/Movilidades/IndexController.php
+++ app/Http/Controllers/Colaboradores/Movilidades/IndexController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Colaboradores\Movilidades;
 
 use App\Colaborador;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MovilidadResource;
 
 class IndexController extends Controller
 {
-    public function __invoke($id){
-        $colaborador=Colaborador::findOrFail($id);
-        return MovilidadResource::collection($colaborador->movilidades->sortByDesc('id'));
+    public function __invoke($id)
+    {
+        $colaborador = Colaborador::findOrFail($id);
+
+        return MovilidadResource::collection($colaborador->movilidades->sortByDesc('fecha_inicio'));
     }
 }
