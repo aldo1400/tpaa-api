ff493ebe4d39813cfa54354110036017d3bd83f3
aldo1400 <achoque1400@gmail.com>
2020-03-23 09:58:33 -0500

    la fecha de inicio y termino de un curso puede coincidir

--- app/Http/Controllers/Cursos/CreateProcessController.php
+++ app/Http/Controllers/Cursos/CreateProcessController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Cursos;
 
 use App\Curso;
-use App\Helpers\Date;
+use Carbon\Carbon;
 use App\Http\Requests\CursoRequest;
 use App\Http\Controllers\Controller;
 
@@ -11,14 +11,16 @@ class CreateProcessController extends Controller
 {
     public function __invoke(CursoRequest $request)
     {
-        if (!Date::revisarFechaDeInicioYTermino($request->fecha_inicio, $request->fecha_termino)) {
-            return response()->json([
+        if ($request->fecha_inicio && $request->fecha_termino) {
+            if (Carbon::parse($request->fecha_termino)->lt(Carbon::parse($request->fecha_inicio))) {
+                return response()->json([
                     'message' => 'Fecha de termino debe ser mayor a la fecha de inicio del curso.',
                     'errors' => [
                         'fecha_inicio' => 'Fecha inválida.',
                         'fecha_termino' => 'Fecha inválida.',
                     ],
                 ], 409);
+            }
         }
 
         $curso = Curso::make($request->validated());
